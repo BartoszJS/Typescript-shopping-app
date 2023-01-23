af79e9859331457ee90bbe7a3de5e4a4c370ad58
BartoszJS <bartosz290300@wp.pl>
2023-01-23 23:19:34 +0100

    30% todo alike

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import "./App.css";
 import ShoppingList from "./components/ShoppingList";
 import Item from "./models/item";
 import ShoppingListForm from "./components/ShoppingListForm";
+import { v4 as getId } from "uuid";
 
 function App() {
   const [items, setItems] = useState<Item[]>([]);
+  const addItem = (product: string, quantity: number) => {
+    console.log("MADE TO APP COMPONENT");
+    setItems([...items, { id: getId(), product, quantity }]);
+  };
   // const items = [
   //   { id: 1, product: "Lemon", quantity: 3 },
   //   { id: 2, product: "Chicken", quantity: 2 },
@@ -13,7 +18,7 @@ function App() {
   return (
     <div>
       <ShoppingList items={items} />
-      <ShoppingListForm />
+      <ShoppingListForm onAddItem={addItem} />
     </div>
   );
 }
--- src/components/ShoppingListForm.tsx
+++ src/components/ShoppingListForm.tsx
@@ -1,18 +1,28 @@
 import React, { useRef } from "react";
 
-function ShoppingListForm(): JSX.Element {
-  const inputRef = React.useRef<HTMLInputElement>(null);
+interface ShoppingListFormProps {
+  onAddItem: (item: string, quantity: number) => void;
+}
+
+function ShoppingListForm({ onAddItem }: ShoppingListFormProps): JSX.Element {
+  const productInputRef = useRef<HTMLInputElement>(null);
+  const quantityInputRef = useRef<HTMLInputElement>(null);
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log(inputRef.current?.value);
+    const newProduct = productInputRef.current!.value;
+    const quantity = parseInt(quantityInputRef.current!.value);
+    onAddItem(newProduct, quantity);
+    productInputRef.current!.value = "";
+    quantityInputRef.current!.value = "1";
   };
   return (
     <form onSubmit={handleSubmit}>
-      <input ref={inputRef} type='text' placeholder='Product name' />
+      <input ref={productInputRef} type='text' placeholder='Product name' />
+      <input ref={quantityInputRef} type='number' min={0} />
       <button type='submit'>Add Item</button>
       {/* <button
         onClick={() => {
-          inputRef.current?.focus();
+          productInputRef.current?.focus();
         }}
       ></button> */}
     </form>
